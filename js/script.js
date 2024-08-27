84ca68e2a430d6cccc81c83855460727730323a7
suminkim8203 <kimsm8203@gmail.com>
2024-08-27 15:41:12 +0900

    docs : 저작권 안내 모달 추가

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+window.onload = function () {
+  // 모달창 기능
+  let modalWrap = document.querySelector(".modal-wrap");
+  let modalClose = document.querySelector(".modal-close");
+  modalClose.addEventListener("click", function () {
+    modalWrap.classList.add("fadeOut");
+    modalWrap.addEventListener("animationend", () => {
+      modalWrap.style.display = "none";
+    });
+  });
+};
+
 window.addEventListener("load", function () {
   AOS.init();
 });
